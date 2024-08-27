b5747cf639a55a1bccc2f7525bcc3f5caf3a69dc
Vishnu Vardhan <vishnuvardhanv046@gmail.com>
2024-08-27 23:10:26 +0530

    Update news.py

--- news.py
+++ news.py
@@ -6,14 +6,3 @@ client = Groq(
     api_key=os.environ.get("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"),
 )
 
-chat_completion = client.chat.completions.create(
-    messages=[
-        {
-            "role": "user",
-            "content": "tell news of today and provide the links of the news display the news date .i need 23-08-2024 news. display the news date",
-        }
-    ],
-    model="llama3-8b-8192",
-)
-
-print(chat_completion.choices[0].message.content)
